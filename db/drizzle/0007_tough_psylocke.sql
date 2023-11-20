b16a35d7512e73e171b2c153ced2b85b88daa66f
nelson <nelson@wizglobal.co.ke>
2023-11-21 01:58:38 +0300

    update: adding forein keys to saved db

---
+++ db/drizzle/0007_tough_psylocke.sql
@@ -0,0 +1,5 @@
+DO $$ BEGIN
+ ALTER TABLE "posts" ADD CONSTRAINT "posts_author_id_user_id_fk" FOREIGN KEY ("author_id") REFERENCES "user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- db/schema/SavedPosts.ts
+++ db/schema/SavedPosts.ts
@@ -1,12 +1,18 @@
 import { relations } from "drizzle-orm";
-import { pgTable, integer, timestamp, primaryKey } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  integer,
+  timestamp,
+  primaryKey,
+  text,
+} from "drizzle-orm/pg-core";
 import { users } from "./users";
 import { Post } from "./post";
 
 export const SavedPost = pgTable(
   "posts",
   {
-    authorId: integer("author_id"),
+    authorId: text("author_id").references(() => users.id),
     postId: integer("post_id"),
     createdAt: timestamp("createdAt").defaultNow(),
   },
